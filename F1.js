a999f73f804635d51bc5761dd3beee5a937c2f60
Jin343 <139105816+Jin343@users.noreply.github.com>
2023-11-21 13:03:40 +0200

    Add files via upload

--- F1.js
+++ F1.js
@@ -6,51 +6,61 @@ window.addEventListener("scroll", () => {
         header.style = "background-color:rgb(0,0,0,0)"
     }
 })
-let request = document.querySelector(".request")
-let input = document.querySelectorAll(".inputs input")
-let button = document.querySelector(".inputs button")
-let sent = document.querySelector(`.sent`)
+// let request = document.querySelector(".request")
+// let input = document.querySelectorAll(".inputs input")
+// let button = document.querySelector(".inputs button")
+// let sent = document.querySelector(`.sent`)
 
-function resetRequest(){
-    input.forEach((e, i) => {
-       e.value=""
-       let required = document.querySelectorAll(`.required`)
-            required[i].classList.add("hid")
-        sent.classList.add("hid")
-    })
-}
-button.addEventListener("click", () => {
-    let number = 0
-    let sent = document.querySelector(`.sent`)
-    input.forEach((e, i) => {
-        if (e.value.length === 0) {
-            number--
-            let required = document.querySelectorAll(`.required`)
-            required[i].classList.remove("hid")
-            sent.classList.add("hid")
-        } else if (e.value.length !== 0) {
-            number++
-            let required = document.querySelectorAll(`.required`)
-            required[i].classList.add("hid")
-        }
-    })
-    if (number === 2) {
-        sent.classList.remove("hid")
-    }
-})
-let specialButton = document.querySelectorAll(".special-button");
-specialButton.forEach((e) => {
-    e.addEventListener("click", () => {
-        request.classList.remove("hid")
-        document.body.classList.add("black-bg")
-        document.body.style.overflow = "hidden";
-        resetRequest()
-    })
+// function resetRequest(){
+//     input.forEach((e, i) => {
+//        e.value=""
+//        let required = document.querySelectorAll(`.required`)
+//             required[i].classList.add("hid")
+//         sent.classList.add("hid")
+//     })
+// }
+// button.addEventListener("click", () => {
+//     let number = 0
+//     let sent = document.querySelector(`.sent`)
+//     input.forEach((e, i) => {
+//         if (e.value.length === 0) {
+//             number--
+//             let required = document.querySelectorAll(`.required`)
+//             required[i].classList.remove("hid")
+//             sent.classList.add("hid")
+//         } else if (e.value.length !== 0) {
+//             number++
+//             let required = document.querySelectorAll(`.required`)
+//             required[i].classList.add("hid")
+//         }
+//     })
+//     if (number === 2) {
+//         sent.classList.remove("hid")
+//     }
+// })
+// let specialButton = document.querySelectorAll(".special-button");
+// specialButton.forEach((e) => {
+//     e.addEventListener("click", () => {
+//         request.classList.remove("hid")
+//         document.body.classList.add("black-bg")
+//         document.body.style.overflow = "hidden";
+//         resetRequest()
+//     })
+// })
+
+// let mark = document.querySelector(".mark")
+// mark.addEventListener("click", () => {
+//     request.classList.add("hid")
+//     document.body.classList.remove("black-bg")
+//     document.body.style.overflow = "auto";
+// })
+
+let links =document.querySelector(".links");
+links.addEventListener("click",()=>{
+    links.classList.add("clicked")
 })
 
-let mark = document.querySelector(".mark")
-mark.addEventListener("click", () => {
-    request.classList.add("hid")
-    document.body.classList.remove("black-bg")
-    document.body.style.overflow = "auto";
+let aboutList =document.querySelector(".about-list")
+aboutList.addEventListener("click",()=>{
+    aboutList.classList.add("clicked-list")
 })
